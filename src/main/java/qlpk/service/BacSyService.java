5ada4f3bb24ca286c462ab17b590a1ef87a31657
Dang-Anh-Tuan <danganhtuan0002@gmail.com>
2021-12-15 22:31:03 +0700

    final cua tuan

---
+++ src/main/java/qlpk/service/BacSyService.java
@@ -0,0 +1,34 @@
+package qlpk.service;
+
+import qlpk.dto.UserDTO;
+import qlpk.entity.BacSy;
+import qlpk.entity.Benh;
+import qlpk.modelUtil.BacSyLuong;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface BacSyService {
+    void deleteBacSy(int id);
+    
+    boolean saveBacSy(BacSy bacSy, UserDTO userDTO);
+    
+    boolean updateBacSy(BacSy bacSy);    
+    
+    List<BacSy> getAll();
+    
+    Optional<BacSy> getById(int id);
+    
+    List<Benh> getListBenhByBacSy(int id);
+    
+    BacSy getByUsername(String username);
+    
+    long getTotalPage();
+    
+    List<BacSy> getByPage(long currentPage);
+    
+    // for tinh luong
+    
+    List<BacSyLuong> tinhLuongBacSy(Date sdate, Date edate);
+}
